12fd814d5c283a7e36294af74aaa74a4c2d47458
olkoa003 <43721193+olkoa003@users.noreply.github.com>
2022-12-26 22:52:18 +0100

    auth is ready

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer>
-            <div>
-                <p>&copy; Chatty 2022. Created by Oleg Kovalenko</p>
-            </div>
-        </footer>
-    )
-}
